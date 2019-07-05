528a38e0f64556033a232b59dcf598dd0e0d5d54
jiangyongsheng <jiangyongsheng@zhongan.com>
2019-07-05 16:35:45 +0800

    add utils

--- src/main/java/com/ej/tools/utils/EjUtils.java
+++ src/main/java/com/ej/tools/utils/EjUtils.java
@@ -3,8 +3,12 @@ package com.ej.tools.utils;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.ej.tools.annotation.ToolsMethod;
+import com.ej.tools.annotation.ToolsParams;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.stream.Stream;
@@ -13,9 +17,10 @@ import java.util.stream.Stream;
 @Component
 public class EjUtils {
 
-    public Object sort(Object obj, String fieldName,boolean reverse) {
-        if (obj instanceof JSONArray) {
-            JSONArray oldArray = (JSONArray) obj;
+    @ToolsMethod("排序")
+    public JSONArray sort(@ToolsParams("待排序对象") Object params, @ToolsParams("排序字段") String fieldName, @ToolsParams("是否倒序[true:倒序;false:正序]") boolean reverse) {
+        if (params instanceof JSONArray) {
+            JSONArray oldArray = (JSONArray) params;
             JSONArray newArray = new JSONArray();
             Stream<Object> stream = oldArray.stream();
             if(reverse){
@@ -32,12 +37,12 @@ public class EjUtils {
             });
             return newArray;
         }
-        throw new RuntimeException("不支持该数据类型[" + obj.getClass().getName() + "]的排序!");
+        throw new RuntimeException("不支持该数据类型[" + params.getClass().getName() + "]的排序!");
     }
-
-    public Object equal(Object obj, String fieldName, String value) {
-        if (obj instanceof JSONArray) {
-            JSONArray oldArray = (JSONArray) obj;
+    @ToolsMethod("等值筛选")
+    public JSONArray equal(@ToolsParams("待筛选数据") Object params, @ToolsParams("筛选字段") String fieldName, @ToolsParams("筛选所需要等于的值") String value) {
+        if (params instanceof JSONArray) {
+            JSONArray oldArray = (JSONArray) params;
             JSONArray newArray = new JSONArray();
             for (int idx = 0; idx < oldArray.size(); idx++) {
                 JSONObject jsonObject = oldArray.getJSONObject(idx);
@@ -47,15 +52,15 @@ public class EjUtils {
             }
             return newArray;
         }
-        throw new RuntimeException("不支持该数据类型[" + obj.getClass().getName() + "]的数据筛选!");
+        throw new RuntimeException("不支持该数据类型[" + params.getClass().getName() + "]的数据筛选!");
     }
-
-    public Object contains(Object obj, String fieldName, String value) {
+    @ToolsMethod("包含值筛选")
+    public JSONArray contains(@ToolsParams("待筛选数据") Object params, @ToolsParams("筛选字段") String fieldName, @ToolsParams("筛选所需要包含的值") String value) {
         if (value == null) {
             throw new RuntimeException("不能通过包含值为null进行筛选，建议使用equal方法！");
         }
-        if (obj instanceof JSONArray) {
-            JSONArray oldArray = (JSONArray) obj;
+        if (params instanceof JSONArray) {
+            JSONArray oldArray = (JSONArray) params;
             JSONArray newArray = new JSONArray();
             for (int idx = 0; idx < oldArray.size(); idx++) {
                 JSONObject jsonObject = oldArray.getJSONObject(idx);
@@ -65,44 +70,45 @@ public class EjUtils {
             }
             return newArray;
         }
-        throw new RuntimeException("不支持该数据类型[" + obj.getClass().getName() + "]的数据筛选！");
+        throw new RuntimeException("不支持该数据类型[" + params.getClass().getName() + "]的数据筛选！");
     }
-
-    public int count(Object obj) {
-        if (obj instanceof JSONArray) {
-            return ((JSONArray) obj).size();
+    @ToolsMethod("统计数据量")
+    public Integer count(@ToolsParams("待统计数据") Object params) {
+        if (params instanceof JSONArray) {
+            return ((JSONArray) params).size();
         }
-        throw new RuntimeException("不支持该数据类型[" + obj.getClass().getName() + "]的数据量统计！");
+        throw new RuntimeException("不支持该数据类型[" + params.getClass().getName() + "]的数据量统计！");
     }
 
-    public BigDecimal sum(Object obj, String... fieldNames) {
-        if (obj instanceof JSONObject) {
-            return sum((JSONObject) obj, fieldNames);
-        } else if (obj instanceof JSONArray) {
-            JSONArray array = (JSONArray) obj;
+    @ToolsMethod("求和")
+    public BigDecimal sum(@ToolsParams("待求和数据") Object params, @ToolsParams("需要求和的字段名列表") String... fieldNames) {
+        if (params instanceof JSONObject) {
+            return sum((JSONObject) params, fieldNames);
+        } else if (params instanceof JSONArray) {
+            JSONArray array = (JSONArray) params;
             BigDecimal sum = BigDecimal.ZERO;
             for (int idx = 0; idx < array.size(); idx++) {
                 sum = sum.add(sum(array.getJSONObject(idx), fieldNames));
             }
             return sum;
         }
-        throw new RuntimeException("不支持该数据类型[" + obj.getClass().getName() + "]的求和！");
+        throw new RuntimeException("不支持该数据类型[" + params.getClass().getName() + "]的求和！");
     }
 
-    private boolean contains(JSONObject obj, String fieldName, String value) {
-        String str = obj.getString(fieldName);
+    private boolean contains(JSONObject params, String fieldName, String value) {
+        String str = params.getString(fieldName);
         return str != null && str.contains(value);
     }
 
-    private boolean equal(JSONObject obj, String fieldName, String value) {
-        return (value == null && obj.getString(fieldName) == null) || (value != null && value.equals(obj.getString(fieldName)));
+    private boolean equal(JSONObject params, String fieldName, String value) {
+        return (value == null && params.getString(fieldName) == null) || (value != null && value.equals(params.getString(fieldName)));
     }
 
-    private BigDecimal sum(JSONObject obj, String... fieldNames) {
+    private BigDecimal sum(JSONObject params, String... fieldNames) {
         BigDecimal sum = BigDecimal.ZERO;
         if (fieldNames != null && fieldNames.length > 0) {
             for (String fieldName : fieldNames) {
-                BigDecimal value = obj.getBigDecimal(fieldName);
+                BigDecimal value = params.getBigDecimal(fieldName);
                 if (value != null) {
                     sum = sum.add(value);
                 }
@@ -110,5 +116,4 @@ public class EjUtils {
         }
         return sum;
     }
-
 }
---
+++ src/main/java/com/ej/tools/web/HelpWeb.java
@@ -0,0 +1,60 @@
+package com.ej.tools.web;
+
+import com.alibaba.fastjson.JSON;
+import com.ej.tools.annotation.ToolsMethod;
+import com.ej.tools.annotation.ToolsParams;
+import com.ej.tools.dto.StringResult;
+import com.ej.tools.utils.EjUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/help")
+@Slf4j
+public class HelpWeb {
+
+    @RequestMapping(value = "/list")
+    @ResponseBody
+    public Object list() {
+        List<Method> methods = Arrays.asList(EjUtils.class.getDeclaredMethods());
+
+        List<Map<String, Object>> result = methods.stream()
+                .filter(method -> method.getAnnotation(ToolsMethod.class) != null)
+                .map(method -> {
+                    ToolsMethod toolsMethod = method.getAnnotation(ToolsMethod.class);
+                    Map<String, Object> map = new LinkedHashMap<>();
+                    map.put("name", toolsMethod.value());
+                    String temp = "%s %s(%s)";
+                    String returnType = method.getReturnType().getName();
+                    String methodName = method.getName();
+                    List<String> show = Arrays.asList(method.getParameters()).stream()
+                            .map(parameter -> parameter.getType().getTypeName() + " " + parameter.getName())
+                            .collect(Collectors.toList());
+                    map.put("method", String.format(temp, returnType, methodName, String.join(",", show)));
+                    final Map<String,Object> info = new LinkedHashMap();
+                    Arrays.asList(method.getParameters()).stream().forEach(parameter -> {
+                        ToolsParams toolsParams = parameter.getAnnotation(ToolsParams.class);
+                        info.put(parameter.getName(),toolsParams.value());
+                    });
+                    map.put("paramsInfo",info);
+                    return map;
+                }).collect(Collectors.toList());
+        return new StringResult().genSuccess(JSON.toJSONString(result, true));
+    }
+
+    public static void main(String[] args) {
+        Object list = new HelpWeb().list();
+
+        System.out.println(JSON.toJSONString(list, true));
+    }
+}
+
